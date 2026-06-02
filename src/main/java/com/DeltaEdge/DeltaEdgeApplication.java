6030a1b8699e5cb9ba2862badc49f49bcf726c02
Learning <sahilgreat321@gmail.com>
2026-06-02 19:31:07 +0530

    grph issue f

--- src/main/java/com/DeltaEdge/DeltaEdgeApplication.java
+++ src/main/java/com/DeltaEdge/DeltaEdgeApplication.java
@@ -1,12 +1,19 @@
 package com.DeltaEdge;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
 public class DeltaEdgeApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeltaEdgeApplication.class, args);
